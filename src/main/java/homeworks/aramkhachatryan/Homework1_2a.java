ab41c72f4b2b913122da84e996ecbdca62c3543f
Aram Khachatryan <aram.g.khachatryan@gmail.com>
2021-07-24 17:03:20 +0400

    Homework1 done

---
+++ src/main/java/homeworks/aramkhachatryan/Homework1_2a.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Homework1_2a {
+    public static void main(String[] args) {
+        float firstNumber;
+        float secondNumber;
+        System.out.print("Please enter first number: ");
+        Scanner input = new Scanner(System.in);
+        firstNumber = input.nextFloat();
+        System.out.print("Please enter second number: ");
+        secondNumber = input.nextFloat();
+        System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+    }
+}
