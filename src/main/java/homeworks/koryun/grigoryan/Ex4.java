b18e74163674b6a0d168eee10b5871d2ea8f07d9
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-07-24 23:32:38 +0400

    Committing Exercise4

--- src/main/java/homeworks/koryun/grigoryan/Ex4.java
+++ src/main/java/homeworks/koryun/grigoryan/Ex4.java
@@ -8,7 +8,7 @@ public class Ex4 {
         System.out.println(side);
         double area = side*side;
         double perimeter = 4*side;
-        System.out.println("The perimeter of the square is "+perimeter);
-        System.out.println("The area of the square is "+area);
+        System.out.println("The perimeter of the square is "+ perimeter);
+        System.out.println("The area of the square is "+ area);
     }
 }
