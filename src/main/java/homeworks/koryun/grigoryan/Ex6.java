d1b564652bcf2aaaf2e9ad664f82453c402945ed
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-07-24 23:44:18 +0400

    Committing Exercise6

---
+++ src/main/java/homeworks/koryun/grigoryan/Ex6.java
@@ -0,0 +1,11 @@
+package homeworks.koryun.grigoryan;
+import java.util.Scanner;
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size in inches.")
+        double inches = input.nextDouble();
+        double meters = inches * 0.0254;
+        System.out.println(inches+" Inches is meters "+meters);
+    }
+}
