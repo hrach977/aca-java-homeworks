ca4a6346680064e078adcaedcea4ee999c6fa6bf
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-07-25 00:04:40 +0400

    Changing the output format

--- src/main/java/homeworks/koryun/grigoryan/Ex5.java
+++ src/main/java/homeworks/koryun/grigoryan/Ex5.java
@@ -6,6 +6,6 @@ public class Ex5 {
         System.out.println("Enter the temperature in Fehrenheit.");
         double fehrenheit= input.nextDouble();
         double celcius = (fehrenheit-32)*5/9;
-        System.out.println("The temperature is "+celcius+"in Celcius.");
+        System.out.println(fehrenheit + " degree in Fehrenheit is equal to "+celcius +"in Celsius");
     }
 }
