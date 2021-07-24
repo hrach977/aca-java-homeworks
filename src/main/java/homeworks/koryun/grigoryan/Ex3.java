71661bb4c5bc034b4abbc95e64f7c61e040bd48a
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-07-24 23:16:24 +0400

    Committing Exercise3

---
+++ src/main/java/homeworks/koryun/grigoryan/Ex3.java
@@ -0,0 +1,16 @@
+package homeworks.koryun.grigoryan;
+import java.util.Scanner;
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = input.nextInt();
+        System.out.format("Here comes the multiplication table of number %d from 1 to 10", num);
+        System.out.println();
+        for(int i = 1;i<=10;i++){
+            System.out.format("%d * %d = "+i*num, num, i);
+            System.out.println();
+        }
+
+    }
+}
