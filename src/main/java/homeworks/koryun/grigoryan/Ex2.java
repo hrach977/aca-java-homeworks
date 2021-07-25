db16f04c53d0996da4a622d01bd57dfd54aa3a8f
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-07-25 15:00:51 +0400

    Committing the first subproblem of exercise2.

--- src/main/java/homeworks/koryun/grigoryan/Ex2.java
+++ src/main/java/homeworks/koryun/grigoryan/Ex2.java
@@ -1,15 +1,15 @@
-package homeworks.koryun.grigoryan;
-import java.util.Scanner;
-public class Ex2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int firstNumber = input.nextInt();
-        System.out.println("Enter another number");
-        int secondNumber = input.nextInt();
-        int int_division = firstNumber/secondNumber;
-        float float_division =((float) firstNumber) / ((float)(secondNumber));
-        System.out.println(firstNumber +" / "+secondNumber+" = "+int_division);
-        System.out.println(firstNumber +" / "+secondNumber+" = "+float_division);
-    }
-}
+//package homeworks.koryun.grigoryan;
+//import java.util.Scanner;
+//public class Ex2 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a number");
+//        int firstNumber = input.nextInt();
+//        System.out.println("Enter another number");
+//        int secondNumber = input.nextInt();
+//        int int_division = firstNumber/secondNumber;
+//        float float_division =((float) firstNumber) / ((float)(secondNumber));
+//        System.out.println(firstNumber +" / "+secondNumber+" = "+int_division);
+//        System.out.println(firstNumber +" / "+secondNumber+" = "+float_division);
+//    }
+//}
---
+++ src/main/java/homeworks/koryun/grigoryan/Ex2A.java
@@ -0,0 +1,15 @@
+package homeworks.koryun.grigoryan;
+import java.util.Scanner;
+public class Ex2A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter another number");
+        int secondNumber = input.nextInt();
+        int int_division = firstNumber/secondNumber;
+//        float float_division =((float) firstNumber) / ((float)(secondNumber));
+//        System.out.println(firstNumber +" / "+secondNumber+" = "+int_division);
+        System.out.println(firstNumber +" / "+secondNumber+" = "+int_division);
+    }
+}
