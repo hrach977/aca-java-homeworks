945697493a6e72ead2f74d80a9bd40f5b457cfa9
Arenkarapetyan <arenkarapetyan19@gmail.com>
2021-07-26 17:07:50 +0400

    home5 ready
    home5 ready

---
+++ src/main/java/homeworks/Home5.java
@@ -0,0 +1,18 @@
+package homeworks;
+
+import java.util.Scanner;
+public class Home5 {
+    public static void main(String[] args) {
+
+        int numer;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print Fahrenheit");
+        numer = scanner.nextInt();
+        System.out.println("In celsius");
+        System.out.println((numer - 32) / 1.8);
+
+
+
+
+    }
+}
