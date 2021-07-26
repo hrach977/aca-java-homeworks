906dfcfeee18419c437a114b99cc164c64efc993
Arenkarapetyan <arenkarapetyan19@gmail.com>
2021-07-26 17:06:13 +0400

    home3 ready
    home3 ready

---
+++ src/main/java/homeworks/Home3.java
@@ -0,0 +1,33 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Home3 {
+    public static void main(String[] args) {
+        int number;
+        int tiv1 = 1;
+        int tiv2 = 2;
+        int tiv3 = 3;
+        int tiv4 = 4;
+        int tiv5 = 5;
+        int tiv6 = 6;
+        int tiv7 = 7;
+        int tiv8 = 8;
+        int tiv9 = 9;
+        int tiv10 = 10;
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        System.out.println(number + ("*") + tiv1 + ("=") + number * tiv1 );
+        System.out.println(number + ("*") + tiv2 + ("=") + number * tiv2 );
+        System.out.println(number + ("*") + tiv3 + ("=") + number * tiv3 );
+        System.out.println(number + ("*") + tiv4 + ("=") + number * tiv4 );
+        System.out.println(number + ("*") + tiv5 + ("=") + number * tiv5 );
+        System.out.println(number + ("*") + tiv6 + ("=") + number * tiv6 );
+        System.out.println(number + ("*") + tiv7 + ("=") + number * tiv7 );
+        System.out.println(number + ("*") + tiv8 + ("=") + number * tiv8 );
+        System.out.println(number + ("*") + tiv9 + ("=") + number * tiv9 );
+        System.out.println(number + ("*") + tiv10 + ("=") + number * tiv10 );
+
+    }}
+
+
