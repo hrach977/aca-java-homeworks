07e9de2a7ede3ba2dd44034ee80d4187751fd4eb
Arenkarapetyan <arenkarapetyan19@gmail.com>
2021-07-26 17:07:18 +0400

    home4 ready
    home4 ready

---
+++ src/main/java/homeworks/Home4.java
@@ -0,0 +1,27 @@
+package homeworks;
+
+import java.util.Scanner;
+
+public class Home4 {
+    public static void main(String[] args) {
+        int number;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print the side of a square");
+        number = scanner.nextInt();
+        System.out.println("The perimeter of the square is");
+        System.out.println(number + number + number + number);
+        System.out.println("The area of the square is");
+        System.out.println( number * number);
+
+
+
+
+
+
+
+
+
+
+    }
+}
