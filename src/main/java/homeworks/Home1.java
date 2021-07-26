0317aeb63671005a14008047928a772f2c8c335a
Arenkarapetyan <arenkarapetyan19@gmail.com>
2021-07-26 16:33:48 +0400

    home1 ready
    home1 ready

--- src/main/java/homeworks/Home1.java
+++ src/main/java/homeworks/Home1.java
@@ -1,6 +1,22 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class Home1 {
 
-    
+    public static void main(String[] args) {
+
+        int numberOne;
+        int numberTwo;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number");
+        numberOne = scanner.nextInt();
+        System.out.println("Second number");
+        numberTwo = scanner.nextInt();
+        System.out.println(numberOne + ("*") + numberTwo + ("=") + numberOne * numberTwo );
+
+
+
+
+    }
 }
