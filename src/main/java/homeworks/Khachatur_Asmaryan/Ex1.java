4ec63e36c117a64683bec9ee745fc26962a071b0
Khachatur6 <87490531+Khachatur6@users.noreply.github.com>
2021-07-27 21:47:27 +0400

    add Khachatur_Asmaryan

---
+++ src/main/java/homeworks/Khachatur_Asmaryan/Ex1.java
@@ -0,0 +1,12 @@
+package homeworks.Khachatur_Asmaryan;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        System.out.println("The first number is " + x);
+        System.out.println("The second number is " + y);
+        System.out.println("10 * 2 = " + ( x * y ));
+
+    }
+}
