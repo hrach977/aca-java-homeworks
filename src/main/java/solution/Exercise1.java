560565735b8092ede7cc60704295fdab48a27c1d
Edmon666 <87490937+Edmon666@users.noreply.github.com>
2021-07-27 19:47:05 +0400

    add solution to the first exercise1

---
+++ src/main/java/solution/Exercise1.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    //Take 2 numbers from input. Print their product.
+    public static void main(String[]args) {
+            Scanner num = new Scanner(System.in);
+            int first, second, result;
+            System.out.println("Exercise1");
+            System.out.println("Enter first num");
+            first = num.nextInt();
+            System.out.println("Enter second num");
+            second = num.nextInt();
+            result = first * second;
+            System.out.println(" Result is " + result);
+        }
+}
