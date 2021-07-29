c3e769a7a103db30c467bc88ae08e9bcb91ab1b5
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-07-29 19:41:51 +0400

    added branch

--- src/main/java/homeworks/artur/danielyan/Test.java
+++ src/main/java/homeworks/artur/danielyan/Test.java
@@ -1,7 +1,22 @@
 package homeworks.artur.danielyan;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int max = a;
+
+        if(max < b){
+            max = b;
+            if(max < c){
+                max = c;
+            }
+        }
+        System.out.println(max);
     }
 }
