2012539a2a1aeb0586de4869e1825d3c933d381c
Ghazaryan1 <anitik1@mail.ru>
2021-07-31 16:50:52 +0400

    Homework1

---
+++ src/main/java/Homeworks/Homework1.java
@@ -0,0 +1,103 @@
+package Homeworks;
+
+import java.util.*;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        //           Exercise 1
+        // Take a number from input between 1-7. Output the corresponding day of week.
+      /* Scanner input = new Scanner(System.in);
+        System.out.println("Please input number");
+       int a = input.nextInt();
+       if(a == 1){
+           System.out.println("Monday");
+       }else if(a == 2){
+           System.out.println("Tuesday");
+       }else if(a == 3){
+           System.out.println("Wednesday");
+       }else if(a == 4){
+           System.out.println("Thursday");
+       }else if(a == 5){
+           System.out.println("Friday");
+       }else if(a == 6){
+           System.out.println("Saturday");
+       }else if(a == 7){
+           System.out.println("Sunday");
+       }
+       else{
+           System.out.println("Invalid day range");
+       }*/
+
+
+//                Exercise 2
+// Solve exercise1 using switch statement
+  /*  Scanner input = new Scanner(System.in);
+        System.out.println("Please input number");
+    int a = input.nextInt();
+    switch(a){
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+        default:
+            System.out.println("invalid day range");
+    }*/
+
+
+//             Exercise 3
+// Solve exercise1 using switch statement.
+
+
+       /* Scanner input = new Scanner(System.in);
+        System.out.println("PLease input first number");
+        double a = input.nextDouble();
+        System.out.println("PLease input second number");
+        double b = input.nextDouble();
+        int num1 = (int)(a*1000);
+        int num2 = (int)(b*1000);
+
+        if(num1 == num2){
+            System.out.println("They are the same up to three decimal places");
+        } else{
+            System.out.println("They are different");
+        }*/
+
+
+//             Exercise 4
+//Take two floating point numbers from input. Check whether they are the same up to three decimal places
+
+
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("PLease input a character");
+        String str = input.nextLine();
+        if (str.length() > 1) {
+            System.out.println("Error. not a single character");
+        } else {
+            char ch = str.charAt(0);
+            if (ch >= 65 && ch <= 90) {
+                System.out.println("Consonant");
+            } else if (ch >= 97 && ch <= 122) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Error, Not a Letter");
+            }
+        }*/
+    }
+}
