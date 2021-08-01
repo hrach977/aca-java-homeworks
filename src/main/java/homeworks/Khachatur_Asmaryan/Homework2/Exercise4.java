e3a4066eceda2b5e9247df364ae436ab2e4a3d73
Khachatur6 <87490531+Khachatur6@users.noreply.github.com>
2021-08-01 22:27:45 +0400

    add Khachatur_Asmaryan

---
+++ src/main/java/homeworks/Khachatur_Asmaryan/Homework2/Exercise4.java
@@ -0,0 +1,20 @@
+package homeworks.Khachatur_Asmaryan.Homework2;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        int year;
+        boolean leap;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter year ");
+        year = input.nextInt();
+        if ( year % 4 == 0) {
+            leap = true;
+            System.out.println(leap);
+        } else {
+            leap = false;
+            System.out.println(leap);
+        }
+    }
+}
