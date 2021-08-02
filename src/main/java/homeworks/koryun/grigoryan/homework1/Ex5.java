560e857d33e36237da176be7e84fdbe787aea284
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-02 15:30:12 +0400

    Committing exercise 5 of homework 1

---
+++ src/main/java/homeworks/koryun/grigoryan/homework1/Ex5.java
@@ -0,0 +1,19 @@
+package homeworks.koryun.grigoryan.homework1;
+import java.util.Scanner;
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year = input.nextInt();
+        if (year%100 == 0){
+            if (year%400 == 0){
+                System.out.println(true);
+            }else{
+                System.out.println(false);
+            }
+        }else if(year%4 == 0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
