6cd6392533f4c4a93f3ac7e611dbfad7de31bb86
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-02 15:18:13 +0400

    Committing exercise 9 of homework 1

---
+++ src/main/java/homeworks/koryun/grigoryan/homework1/Ex9.java
@@ -0,0 +1,10 @@
+package homeworks.koryun.grigoryan.homework1;
+import java.util.Scanner;
+public class Ex9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean bool1 = input.nextBoolean();
+        boolean bool2 = input.nextBoolean();
+        System.out.println(bool1==bool2);
+    }
+}
