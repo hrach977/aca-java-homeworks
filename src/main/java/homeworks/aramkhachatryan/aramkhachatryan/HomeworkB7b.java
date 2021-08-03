a911ed3623cffd07986807cbcacef9f1bfe4d0ad
Aram Khachatryan <aram.g.khachatryan@gmail.com>
2021-08-03 20:36:03 +0400

    Homework corrections

--- src/main/java/homeworks/aramkhachatryan/aramkhachatryan/HomeworkB7b.java
+++ src/main/java/homeworks/aramkhachatryan/aramkhachatryan/HomeworkB7b.java
@@ -1,12 +1,12 @@
-//  Take two integers from input. Output true if either of them is in the range 10..20 inclusive.
-//  Test Data: 12, 1000
-//  Expected Output: True
+//  Take two numbers from input. Output true if one of them is 10 or if their sum is 10.
+//  Test Data: 2, 8
+//  Expected Output: true
 
 package aramkhachatryan;
 
 import java.util.Scanner;
 
-public class HomeworkB8 {
+public class HomeworkB7b {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter two integer numbers.");
@@ -14,6 +14,6 @@ public class HomeworkB8 {
         int num1 = input.nextInt();
         System.out.print("B: ");
         int num2 = input.nextInt();
-        System.out.println((num1 >= 10 && num1 <= 20) || (num2 >= 10 && num2 <= 20));
+        System.out.println((num1 == 10) || (num2 == 10) || (num1 + num2 == 10));
     }
 }
