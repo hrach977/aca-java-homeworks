c1ddb90a16ddcfa136c798c94f16deedef63cc73
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-03 20:12:09 +0400

    ex5 changed

--- src/main/java/homeworks/artur/danielyan/Exercise5.java
+++ src/main/java/homeworks/artur/danielyan/Exercise5.java
@@ -8,7 +8,9 @@ public class Exercise5 {
 
         System.out.println("Please enter a year");
         int year = input.nextInt();
-        if(year%4 == 0){
+        boolean first = (year%4 == 0) && (year%100 != 0);
+        boolean second = year%400 == 0;
+        if(first || second){
             System.out.println("Entered year is a leap year");
         }else{
             System.out.println("Entered year isn't a leap year");
