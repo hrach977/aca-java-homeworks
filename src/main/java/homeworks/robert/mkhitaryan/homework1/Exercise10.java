7396400bc454435b2aaed26bf81b70b2c1485518
robertmkh <87925836+robertmkh@users.noreply.github.com>
2021-08-03 13:01:23 +0400

    Update Exercise10.java

--- src/main/java/homeworks/robert/mkhitaryan/homework1/Exercise10.java
+++ src/main/java/homeworks/robert/mkhitaryan/homework1/Exercise10.java
@@ -8,21 +8,21 @@ public class Exercise10 {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter the month: ");
         String month = input.next();
-        System.out.print("\nPlease enter the year: ");
+        System.out.print("Please enter the year: ");
         short year = input.nextShort();
         boolean isMonthHaveThirtyDays = ((month.equals("April")) || (month.equals("June")) ||
                                         (month.equals("September")) || (month.equals("November")));
         if (isMonthHaveThirtyDays) {
-            System.out.println("The month have 30 days.");
+            System.out.println("The month has 30 days.");
         } else if (month.equals("February")) {
             boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
             if(!isLeapYear) {
-                System.out.println("The month have 28 days.");
+                System.out.println("The month has 28 days.");
             } else {
-                System.out.println("The month have 29 days.");
+                System.out.println("The month has 29 days.");
             }
         } else {
-            System.out.println("The month have 31 days.");
+            System.out.println("The month has 31 days.");
         }
     }
 }
