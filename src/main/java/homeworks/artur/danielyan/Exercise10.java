6f357a0ed4eb99941bbe4fe25d2d606f5397df45
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-03 21:14:59 +0400

    ex 10 and 11 changed

--- src/main/java/homeworks/artur/danielyan/Exercise10.java
+++ src/main/java/homeworks/artur/danielyan/Exercise10.java
@@ -1,5 +1,6 @@
 package homeworks.artur.danielyan;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise10 {
@@ -8,25 +9,25 @@ public class Exercise10 {
 
         System.out.println("Please enter month");
         String month = input.nextLine();
-
-        if(!(month.equals("January") || month.equals("March") || month.equals("May") || month.equals("July") || month.equals("August") || month.equals("October") || month.equals("December") || month.equals("April") || month.equals("June") || month.equals("September") || month.equals("November") || month.equals("February"))){
-            System.out.println("invalid month");
-        }else {
-
-            System.out.println("Please enter year");
-            int year = input.nextInt();
-            if (month.equals("February")) {
-                if (year % 4 == 0) {
+        System.out.println("Please enter year");
+        int year = input.nextInt();
+        month = month.toLowerCase();
+        if(month.equals("january") || month.equals("february") || month.equals("march") || month.equals("april") || month.equals("may") || month.equals("june") || month.equals("july") || month.equals("august") || month.equals("september") || month.equals("october") || month.equals("november") || month.equals("december")){
+            if(month.equals("january") || month.equals("march") || month.equals("may") || month.equals("july") || month.equals("august") || month.equals("october") || month.equals("december")){
+                System.out.println(31);
+            }else if (month.equals("february")){
+                boolean first = (year % 4 == 0) && (year % 100 != 0);
+                boolean second = year % 400 == 0;
+                if (first || second) {
                     System.out.println(29);
                 } else {
                     System.out.println(28);
                 }
-            } else if (month.equals("January") || month.equals("March") || month.equals("May") || month.equals("July") || month.equals("August") || month.equals("October") || month.equals("December")) {
-                System.out.println(31);
-            } else {
+            }else{
                 System.out.println(30);
             }
+        }else{
+            System.out.println("Invalid month");
         }
-
     }
 }
--- src/main/java/homeworks/artur/danielyan/Exercise11.java
+++ src/main/java/homeworks/artur/danielyan/Exercise11.java
@@ -6,35 +6,41 @@ public class Exercise11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Please enter month");
         String month = input.nextLine();
-        if(!(month.equals("January") || month.equals("March") || month.equals("May") || month.equals("July") || month.equals("August") || month.equals("October") || month.equals("December") || month.equals("April") || month.equals("June") || month.equals("September") || month.equals("November") || month.equals("February"))){
-            System.out.println("invalid month");
-        }else{
-            int year = input.nextInt();
-            switch (month){
-                case "February": {
-                    if(year%4 == 0){
+        System.out.println("Please enter year");
+        int year = input.nextInt();
+        month = month.toLowerCase();
+
+        if(month.equals("january") || month.equals("february") || month.equals("march") || month.equals("april") || month.equals("may") || month.equals("june") || month.equals("july") || month.equals("august") || month.equals("september") || month.equals("october") || month.equals("november") || month.equals("december")) {
+            switch (month) {
+                case "january":
+                case "march":
+                case "may":
+                case "july":
+                case "august":
+                case "october":
+                case "december": {
+                    System.out.println(31);
+                    break;
+                }
+                case "february": {
+                    boolean first = (year % 4 == 0) && (year % 100 != 0);
+                    boolean second = year % 400 == 0;
+                    if (first || second) {
                         System.out.println(29);
-                    }else {
+                    } else {
                         System.out.println(28);
                     }
                     break;
                 }
-                case "January":
-                case "March":
-                case "May":
-                case "July":
-                case "August":
-                case "October":
-                case "December":
-                {
-                    System.out.println(31);
-                    break;
-                }
                 default: {
                     System.out.println(30);
+                    break;
                 }
             }
+        }else{
+            System.out.println("Invalid month");
         }
     }
 }
