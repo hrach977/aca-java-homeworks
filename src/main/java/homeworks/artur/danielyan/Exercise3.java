35dca0e611a189657e340f463add629f4fd0988a
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-03 19:46:50 +0400

    changed ex3

--- src/main/java/homeworks/artur/danielyan/Exercise3.java
+++ src/main/java/homeworks/artur/danielyan/Exercise3.java
@@ -8,9 +8,9 @@ public class Exercise3 {
 
         double a = input.nextDouble();
         double b = input.nextDouble();
-        int a1 = (int)(a*1000);
-        int b1 = (int)(b*1000);
-        if(a1 == b1){
+        a = Math.floor(a * 1000);
+        b = Math.floor(b * 1000);
+        if(a == b){
             System.out.println("They are the same up to three decimal places");
         }else{
             System.out.println("They are different");
