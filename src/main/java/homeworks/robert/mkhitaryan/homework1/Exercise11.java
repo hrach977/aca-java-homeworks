6ce44cc239c5b565c72bf62ae1195f43ed6f66c7
robertmkh <87925836+robertmkh@users.noreply.github.com>
2021-08-03 13:00:06 +0400

    Update Exercise11.java

--- src/main/java/homeworks/robert/mkhitaryan/homework1/Exercise11.java
+++ src/main/java/homeworks/robert/mkhitaryan/homework1/Exercise11.java
@@ -12,16 +12,16 @@ public class Exercise11 {
         short year = input.nextShort();
 
         switch (month) {
-            case "April", "June", "September", "November" -> System.out.println("The month have 30 days.");
+            case "April", "June", "September", "November" -> System.out.println("The month has 30 days.");
             case "February" -> {
                 boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
                 if (!isLeapYear) {
-                    System.out.println("The month have 28 days.");
+                    System.out.println("The month has 28 days.");
                 } else {
-                    System.out.println("The month have 29 days.");
+                    System.out.println("The month has 29 days.");
                 }
             }
-            default -> System.out.println("The month have 31 days.");
+            default -> System.out.println("The month has 31 days.");
         }
     }
 }
