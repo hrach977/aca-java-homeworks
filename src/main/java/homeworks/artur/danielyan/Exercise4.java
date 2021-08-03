6a6ec7e419bed20dc4d9b582a69da331dfd2c4de
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-03 20:02:49 +0400

    ex4 single ch converted to lower case

--- src/main/java/homeworks/artur/danielyan/Exercise4.java
+++ src/main/java/homeworks/artur/danielyan/Exercise4.java
@@ -8,17 +8,13 @@ public class Exercise4 {
 
         System.out.println("Please enter a single letter");
         String str = input.nextLine();
+        str = str.toLowerCase();
         if(str.length() > 1){
             System.out.println("Error. Not a single character.");
         }else{
             char ch = str.charAt(0);
-            if( (ch>=65 && ch<=90) || (ch>=97 && ch<=122) ){
-                int v1 = 65;
-                int v2 = 69;
-                int v3 = 73;
-                int v4 = 79;
-                int v5 = 85;
-                if((ch == v1 || ch == v1 + 32) || (ch == v2 || ch == v2 + 32) || (ch == v3 || ch == v3 + 32) || (ch == v4 || ch == v4 + 32) || (ch == v5 || ch == v5 + 32) ){
+            if( (ch>=97 && ch<=122) ){
+                if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117){
                     System.out.println("Vowel");
                 }else{
                     System.out.println("Consonant");
