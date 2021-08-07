ceefe5d1db6f31fc2aad0f9167380d89119cea6a
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-07 22:28:57 +0400

    ex5 isn't done

--- src/main/java/homeworks/artur/danielyan/homework2/Exercise5.java
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise5.java
@@ -2,6 +2,6 @@ package homeworks.artur.danielyan.homework2;
 
 public class Exercise5 {
     public static void main(String[] args) {
-        //???????
+        //????????
     }
 }
--- src/main/java/homeworks/artur/danielyan/homework2/Exercise6.java
+++
@@ -1,4 +0,0 @@
-package homeworks.artur.danielyan.homework2;
-
-public class Exercise6 {
-}
