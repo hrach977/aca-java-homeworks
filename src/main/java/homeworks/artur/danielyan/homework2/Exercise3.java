6c718ea9848b6bffe5f1fc0f087a2f79de4e1b0a
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-07 20:39:15 +0400

    ex3 added

---
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise3.java
@@ -0,0 +1,21 @@
+package homeworks.artur.danielyan.homework2;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the term");
+        int n = input.nextInt();
+        int[] fiboncci = new int[n];
+        fiboncci[0] = 0;
+        fiboncci[1] = 1;
+
+        for(int i = 2; i < n; i++){
+            fiboncci[i] = fiboncci[i-2] + fiboncci[i-1];
+        }
+        for(int num : fiboncci){
+            System.out.println(num);
+        }
+    }
+}
