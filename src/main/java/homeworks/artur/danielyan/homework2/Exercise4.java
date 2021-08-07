334839e6a7f9b26a9db2d2eaf2b4b874f1094c53
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-07 21:33:06 +0400

    ex4 done

---
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise4.java
@@ -0,0 +1,17 @@
+package homeworks.artur.danielyan.homework2;
+
+import java.util.Scanner;
+public class Exercise4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the number");
+        int n = input.nextInt();
+        double result = 0;
+
+        for(int i = 1; i <= n; i++){
+            result += 1d/i;
+        }
+        System.out.println(result);
+    }
+}
