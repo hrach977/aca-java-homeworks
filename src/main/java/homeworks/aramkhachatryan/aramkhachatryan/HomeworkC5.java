da90dfe845dcc4d4c9c1807db2438e7e8dafc7bf
Aram Khachatryan <aram.g.khachatryan@gmail.com>
2021-08-09 00:44:45 +0400

    Homework C1-C4

--- src/main/java/homeworks/aramkhachatryan/aramkhachatryan/HomeworkC5.java
+++ src/main/java/homeworks/aramkhachatryan/aramkhachatryan/HomeworkC5.java
@@ -1,23 +1,24 @@
-//Write a program to calculate the sum of the following series,
-// where n is input by user:1 +½+ 1/3 + ¼+ 1/5 + 1/6 + .... + 1/n
+//Compute the natural logarithm of 2, by adding up to n terms in the series:
+// 1 –½+ 1/3 –¼+ 1/5 -...1/n  ,where n is a positive integer input by user.
 
 package aramkhachatryan;
 
 import java.util.Scanner;
 
-public class HomeworkC4 {
+public class HomeworkC5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter n number: ");
         int n = input.nextInt();
         double sum = 1;
-        if (n==1){
+        if (n == 1) {
             System.out.println(sum);
         } else {
             for (double i = 2; i <= n; i++) {
-                sum = sum + 1 / i;
+                sum = (sum - 1 / i);
+                sum *= (-1);
             }
-            System.out.println(sum);
+            System.out.println(Math.abs(sum));
         }
     }
 }
