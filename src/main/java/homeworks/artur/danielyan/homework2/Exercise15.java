8f4af3ccce953fc8a9a559a638d41b9e20ddd48f
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-08 19:24:06 +0400

    ex15 done

---
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise15.java
@@ -0,0 +1,21 @@
+package homeworks.artur.danielyan.homework2;
+
+import java.util.Scanner;
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] arr = {1,4,3,15,24,99,96,85,9,26,12,18,30,0,10,8,2,1};// check with 100 or 30 or 10
+
+        System.out.println("Please enter the sum number");
+        int sum = input.nextInt();
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j < arr.length; j++){
+                if(arr[i] + arr[j] == sum){
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + (arr[i] + arr[j]));
+                }
+            }
+        }
+    }
+}
