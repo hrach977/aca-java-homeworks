f558402f1638dd5ee1c7239d5675aa41335f967f
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-08 18:11:10 +0400

    ex5 done

--- src/main/java/homeworks/artur/danielyan/homework2/Exercise5.java
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise5.java
@@ -1,7 +1,21 @@
 package homeworks.artur.danielyan.homework2;
 
+import java.util.Scanner;
+
 public class Exercise5 {
     public static void main(String[] args) {
-        //????????
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the number");
+        int n = input.nextInt();
+        double result = 0;
+        for(int i = 1; i <= n; i++){
+            if(i%2 == 0){
+                result -= 1d/i;
+            }else{
+                result += 1d/i;
+            }
+        }
+        System.out.println(result);
     }
 }
