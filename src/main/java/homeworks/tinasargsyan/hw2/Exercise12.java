f1c49de75d7e6b946c073d465125a0e05dc6c6fb
Christina-95 <christinasargsyan@gmail.com>
2021-08-10 02:24:59 +0400

    exercises 12 added

---
+++ src/main/java/homeworks/tinasargsyan/hw2/Exercise12.java
@@ -0,0 +1,24 @@
+package homeworks.tinasargsyan.hw2;
+/*
+Given an array of ints,
+output true if
+the sum of all the ‘2’-s in the array is exactly ‘8’.
+ */
+public class Exercise12 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,2,5,2,2};
+        boolean exactlyEight = true;
+        int sum = 0;
+        for(int i = 0;i< arr.length;i++){
+            if(arr[i]==2){
+                sum+=arr[i];
+            }
+            if(sum==8){
+                exactlyEight=true;
+            }else{
+                exactlyEight=false;
+            }
+        }
+        System.out.println(exactlyEight);
+    }
+}
