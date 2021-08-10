f8dc98ce4829c2e0dd873c2ad6118794b40637c7
Christina-95 <christinasargsyan@gmail.com>
2021-08-10 15:12:19 +0400

    exercise 13 added

---
+++ src/main/java/homeworks/tinasargsyan/hw2/Exercise13.java
@@ -0,0 +1,19 @@
+package homeworks.tinasargsyan.hw2;
+/*
+Write a program to reverse the elements of an integer array (1 dimensional).
+ */
+public class Exercise13 {
+    public static void main(String[] args) {
+        int [] arr = {4,5,3,2,9};
+        System.out.println("Original Array ");
+        for(int i=0;i<arr.length;i++)
+            System.out.print(arr[i] + "  ");
+
+        System.out.println();
+
+        System.out.println("Reversed array ");
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i] + "  ");
+        }
+    }
+}
