0465f85010d6847c38ef5818d0eb62e6f1eae560
Christina-95 <christinasargsyan@gmail.com>
2021-08-10 20:39:19 +0400

    correction in ex. 6

--- src/main/java/homeworks/tinasargsyan/hw2/Exercise6.java
+++ src/main/java/homeworks/tinasargsyan/hw2/Exercise6.java
@@ -3,28 +3,17 @@ package homeworks.tinasargsyan.hw2;
 Given an array of ints,
 output true if the array contains no 1’s and no 3’s.
  */
-import java.util.Random;
-import java.util.Scanner;
-
 public class Exercise6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the length of the array");
-        int n = sc.nextInt();
-        System.out.println("Please enter the limit of elements values of the array");
-        int limit = sc.nextInt();
 
-        boolean flag=false;
+        int arr[] = {1,2,3,4,5,7};
+        boolean flag=true;
 
-        Random rand = new Random();
-        int arr[] = new int[n];
-        for (int i=0;i< arr.length;i++){
-            arr[i] = rand.nextInt(limit);
-            System.out.println(arr[i]);
-            if((arr[i]==1) && (arr[i]==3)){
+
+        for (int element : arr){
+            if(element ==1 || element ==3){
                 flag=false;
-            }else{
-                flag = true;
+                break;
             }
         }
 
