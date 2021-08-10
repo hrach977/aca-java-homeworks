c73c461c3f7e0a55f087f20252eede1803bc487c
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-10 21:28:22 +0400

    ex8 added length=0 case

--- src/main/java/homeworks/artur/danielyan/homework2/Exercise8.java
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise8.java
@@ -2,18 +2,22 @@ package homeworks.artur.danielyan.homework2;
 
 public class Exercise8 {
     public static void main(String[] args) {
-        int[] arr = {6,2,5,3};
+        int[] arr = {};
 
-        int save = arr[0];
-        for(int i = 0; i < arr.length - 1; i++){
-            arr[i] = arr[i+1];
+        if(arr.length != 0) {
+            int save = arr[0];
+            for (int i = 0; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+
+            arr[arr.length - 1] = save;
+
+            for (int e : arr) {
+                System.out.print(e + " ");
+            }
+            System.out.println();
+        }else{
+            System.out.println("Array doesn't contain anything");
         }
-
-        arr[arr.length-1] = save;
-
-        for(int e : arr){
-            System.out.print(e + " ");
-        }
-        System.out.println();
     }
 }
