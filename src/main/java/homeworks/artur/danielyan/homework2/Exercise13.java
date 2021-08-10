e981e475b0669cf1329d5b681b29a44128d97e1e
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-10 21:57:43 +0400

    ex13 without new array(I'm stupid)

--- src/main/java/homeworks/artur/danielyan/homework2/Exercise13.java
+++ src/main/java/homeworks/artur/danielyan/homework2/Exercise13.java
@@ -2,17 +2,17 @@ package homeworks.artur.danielyan.homework2;
 
 public class Exercise13 {
     public static void main(String[] args) {
-        int[] arr = {1,2,5,78,56,1,45,8,120,3};
+        int[] arr = {1,2,5,78,56,1,45,8,120};
 
-        int[] reversed = new int[arr.length];
-
-        for(int i = arr.length-1; i >= 0; i--){
-            reversed[i] = arr[arr.length-1-i];
+        int temp;
+        for(int i = 0; i < arr.length/2; i++){
+            temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
         }
 
-        for(int e : reversed){
+        for(int e : arr){
             System.out.print(e + " ");
         }
-        System.out.println();
     }
 }
