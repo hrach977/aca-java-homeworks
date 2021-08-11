3c6562db066d387921a5d2c477807a3c0bca0deb
Christina-95 <christinasargsyan@gmail.com>
2021-08-11 23:51:13 +0400

    correction in ex. 7

--- src/main/java/homeworks/tinasargsyan/hw2/Exercise7.java
+++ src/main/java/homeworks/tinasargsyan/hw2/Exercise7.java
@@ -8,9 +8,19 @@ import java.util.Arrays;
 public class Exercise7 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
-        int[] arr1 = {arr[1], arr[2], arr[3],arr[0]};
-        System.out.println("The original array " + Arrays.toString(arr));
-        System.out.println("New array " + Arrays.toString(arr1));
+//        int[] arr1 = {arr[1], arr[2], arr[3],arr[0]};
+//        System.out.println("The original array " + Arrays.toString(arr));
+//        System.out.println("New array " + Arrays.toString(arr1));
 
+        if(arr.length!=0){
+            int[] res = new int [arr.length];
+            for(int i=1;i<arr.length;i++){
+                res[i-1] = arr[i];
+            }
+            res[res.length-1] = arr[0];
+            for(int element : res){
+                System.out.print(element + " ");
+            }
+        }
     }
 }
