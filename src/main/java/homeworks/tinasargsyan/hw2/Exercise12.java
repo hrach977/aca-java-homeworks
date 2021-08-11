3eca70debf150082fd325f237e3206a23f6c9b8b
Christina-95 <christinasargsyan@gmail.com>
2021-08-11 23:56:41 +0400

    v2 added in ex. 12

--- src/main/java/homeworks/tinasargsyan/hw2/Exercise12.java
+++ src/main/java/homeworks/tinasargsyan/hw2/Exercise12.java
@@ -9,16 +9,26 @@ public class Exercise12 {
         int[] arr = {1,2,3,4,2,5,2,2};
         boolean exactlyEight = true;
         int sum = 0;
-        for(int i = 0;i< arr.length;i++){
-            if(arr[i]==2){
-                sum+=arr[i];
-            }
-            if(sum==8){
-                exactlyEight=true;
-            }else{
-                exactlyEight=false;
+//        for(int i = 0;i< arr.length;i++){
+//            if(arr[i]==2){
+//                sum+=arr[i];
+//            }
+//            if(sum==8){
+//                exactlyEight=true;
+//            }else{
+//                exactlyEight=false;
+//            }
+//        }
+//       System.out.println(exactlyEight);
+
+
+        //V2
+        for(int element : arr){
+            if(element ==2){
+                sum+=element;
             }
         }
-        System.out.println(exactlyEight);
+        System.out.println(sum==8);
+
     }
 }
