ead6a59bc072e340b559615600096298c24beaa4
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-13 20:14:26 +0400

    ex5 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise5.java
@@ -0,0 +1,11 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println(endsInLY("quietly"));
+    }
+
+    public static boolean endsInLY(String word){
+        return word.endsWith("ly");
+    }
+}
