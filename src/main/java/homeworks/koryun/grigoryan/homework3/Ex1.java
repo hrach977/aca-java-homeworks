6370983f3395484f98df391ac30b723aaf058ad4
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-13 16:34:22 +0400

    Committing exercise 1 of homework 3

---
+++ src/main/java/homeworks/koryun/grigoryan/homework3/Ex1.java
@@ -0,0 +1,10 @@
+package homeworks.koryun.grigoryan.homework3;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println(greet("John"));
+    }
+    public static String greet(String name){
+        return "Hello "+name+"!";
+    }
+}
