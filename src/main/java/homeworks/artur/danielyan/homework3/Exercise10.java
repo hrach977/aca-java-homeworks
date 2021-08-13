d0c17d4d3dfbfc5ddf5404ec9f6f386235cabe0d
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-13 22:52:35 +0400

    ex10 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise10.java
@@ -0,0 +1,23 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise10 {
+    public static void main(String[] args) {
+        System.out.println(threeOfFirstTwo("Hello!"));
+        System.out.println(threeOfFirstTwo("He"));
+        System.out.println(threeOfFirstTwo("H"));
+        System.out.println(threeOfFirstTwo(""));
+    }
+
+    public static String threeOfFirstTwo(String str){
+        String firstTwo;
+        if (str.length() >= 2){
+            firstTwo = str.substring(0,2);
+        }else if (str.length() == 1){
+            firstTwo = str;
+        }else{
+            firstTwo = "";
+        }
+
+        return firstTwo + firstTwo + firstTwo;
+    }
+}
