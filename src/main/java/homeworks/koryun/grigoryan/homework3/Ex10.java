919851be069a253b50fcb44fc7a8c14c8ab03563
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-13 17:02:40 +0400

    Committing exercise 10 of homework 3

---
+++ src/main/java/homeworks/koryun/grigoryan/homework3/Ex10.java
@@ -0,0 +1,16 @@
+package homeworks.koryun.grigoryan.homework3;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        System.out.println(generateString("hello"));
+        System.out.println(generateString("mountain"));
+        System.out.println(generateString("a"));
+        System.out.println(generateString("ab"));
+    }
+    public static String generateString(String str){
+        if (str.length()<2){
+            return str;
+        }
+        return str.substring(0,2)+str.substring(0,2)+str.substring(0,2);
+    }
+}
