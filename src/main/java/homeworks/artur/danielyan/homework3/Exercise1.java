66ee176dc05a5b3ab69b4c2b7d67ed9155b163dc
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-13 19:27:14 +0400

    ex1 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise1.java
@@ -0,0 +1,11 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println(greet("Artur"));
+    }
+
+    public static String greet(String name){
+        return "Hello " + name + "!";
+    }
+}
