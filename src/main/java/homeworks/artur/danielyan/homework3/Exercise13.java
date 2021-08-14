7e9a65a25612b8432b93682392cfc2b06e526b1d
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-14 14:44:38 +0400

    ex13 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise13.java
@@ -0,0 +1,14 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise13 {
+    public static void main(String[] args) {
+        System.out.println(threeOfLastTwo("Hello"));
+        System.out.println(threeOfLastTwo("Hi"));
+    }
+
+    public static String threeOfLastTwo(String str){
+        if(str.length() == 2) return str + str + str;
+        String substring = str.substring(str.length()-2);
+        return substring + substring + substring;
+    }
+}
