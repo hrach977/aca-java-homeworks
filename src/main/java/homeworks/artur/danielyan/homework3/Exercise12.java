0a8726fc24acbba7547eb888fdc2a3275fc2c55d
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-14 14:39:06 +0400

    ex12 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise12.java
@@ -0,0 +1,11 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+        System.out.println(abba("Hi","Bye"));
+    }
+
+    public static String abba(String a, String b){
+        return a+b+b+a;
+    }
+}
