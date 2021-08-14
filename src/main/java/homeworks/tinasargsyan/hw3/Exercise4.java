84dbea66243e92fc7889cc83bcac3fcd41d04f80
Christina-95 <christinasargsyan@gmail.com>
2021-08-15 01:28:59 +0400

    correction in ex. 4 added

--- src/main/java/homeworks/tinasargsyan/hw3/Exercise4.java
+++ src/main/java/homeworks/tinasargsyan/hw3/Exercise4.java
@@ -6,15 +6,17 @@ The strings will be at least length 1.
  */
 public class Exercise4 {
     public static void main(String[] args) {
-        String first = "Hhello";
-        String second = "Ffriend";
+        String first = "KKK";
+        String second = "BBB";
         System.out.println(concatStrings(first,second));
     }
 
     public static String concatStrings(String first, String second){
         String concat= " ";
-        if(first.substring(1).length()>1 && second.substring(1).length()>1){
+        if(first.substring(1).length()>=1 && second.substring(1).length()>=1){
             concat = (first.substring(1) + second.substring(1));
+        }else{
+            System.out.println("String length should be 1 or more");
         }
         return concat;
     }
