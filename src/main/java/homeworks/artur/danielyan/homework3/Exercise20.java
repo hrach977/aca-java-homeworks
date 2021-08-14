c1fe0bd1d761a8909188fcf9bfe2a91c6fff3bf6
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-14 15:52:39 +0400

    ex20 done

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise20.java
@@ -0,0 +1,21 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise20 {
+    public static void main(String[] args) {
+        System.out.println(howManyHi("hi something hi hi texttext hi hi"));//6
+        System.out.println(howManyHi("hi hi"));//2
+        System.out.println(howManyHi(""));//0
+        System.out.println(howManyHi("hi"));//1
+        System.out.println(howManyHi(" hi "));//1
+    }
+
+    public static int howManyHi(String str){
+        int count = 0;
+        for(int i = 0; i <= str.length()-2; i++){
+            if(str.charAt(i) == 'h' && str.charAt(i+1) == 'i'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
