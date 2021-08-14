6223be9a6e92d279845811d8fb22985ff66efef6
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-14 16:26:29 +0400

    Homework done!

---
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise23.java
@@ -0,0 +1,26 @@
+package homeworks.artur.danielyan.homework3;
+
+public class Exercise23 {
+    public static void main(String[] args) {
+        System.out.println(isEqualDogsAndCats("erjbg dog jgf dog irgwh catgwg cat"));//true
+        System.out.println(isEqualDogsAndCats("nerg dog herfu cat grjwigdog"));//false
+        System.out.println(isEqualDogsAndCats("dog cat"));//true
+        System.out.println(isEqualDogsAndCats("cat reghsgn dog gqgnca"));//true
+        System.out.println(isEqualDogsAndCats("dog isn't a cat dog is a dog"));//false
+    }
+
+    public static boolean isEqualDogsAndCats(String str){
+        int cat = 0;
+        int dog = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.startsWith("dog", i)){
+                dog++;
+            }else if(str.startsWith("cat", i)){
+                cat++;
+            }
+        }
+
+        return cat==dog;
+    }
+}
