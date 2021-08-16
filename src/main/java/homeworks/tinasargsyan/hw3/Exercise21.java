6500860c64245e64e33fdf66874a62de84a861c8
Christina-95 <christinasargsyan@gmail.com>
2021-08-17 01:49:14 +0400

    exercise 21 added

---
+++ src/main/java/homeworks/tinasargsyan/hw3/Exercise21.java
@@ -0,0 +1,30 @@
+package homeworks.tinasargsyan.hw3;
+/*
+Given two strings,
+return true
+if either of the strings appears at the very end of the other string,
+ignoring upper/lower case differences
+(in other words, the computation should not be "case sensitive").
+ */
+public class Exercise21 {
+    public static void main(String[] args) {
+        System.out.println(endString("Good","daygood"));
+        System.out.println(endString("hi","byehi"));
+    }
+
+    public static boolean endString(String str1, String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if(str1.length() < str2.length()){
+            String tmp = str2.substring(str2.length()-str1.length(),str2.length());
+            if(tmp.compareTo(str1)==0){
+                return true;
+            }return false;
+        }else{
+            String tmp = str1.substring(str1.length()-str2.length(),str1.length());
+            if(tmp.compareTo(str2)==0){
+                return true;
+            }return false;
+        }
+    }
+}
