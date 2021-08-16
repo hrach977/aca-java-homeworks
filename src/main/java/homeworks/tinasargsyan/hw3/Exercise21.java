c6da732cac439a3051a30216dde73d62def3569d
Christina-95 <christinasargsyan@gmail.com>
2021-08-17 01:50:27 +0400

    correction in ex. 21

--- src/main/java/homeworks/tinasargsyan/hw3/Exercise21.java
+++ src/main/java/homeworks/tinasargsyan/hw3/Exercise21.java
@@ -16,12 +16,12 @@ public class Exercise21 {
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
         if(str1.length() < str2.length()){
-            String tmp = str2.substring(str2.length()-str1.length(),str2.length());
+            String tmp = str2.substring(str2.length()-str1.length());
             if(tmp.compareTo(str1)==0){
                 return true;
             }return false;
         }else{
-            String tmp = str1.substring(str1.length()-str2.length(),str1.length());
+            String tmp = str1.substring(str1.length()-str2.length());
             if(tmp.compareTo(str2)==0){
                 return true;
             }return false;
