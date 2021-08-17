f22e8b68a9b5747e1d1c1e3dcb421556f702cd72
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-17 19:29:18 +0400

    ex3 removed check

--- src/main/java/homeworks/artur/danielyan/homework3/Exercise3.java
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise3.java
@@ -6,10 +6,6 @@ public class Exercise3 {
     }
 
     public static String cutInHalf(String cutThis){
-        if(cutThis.length()%2 == 1){
-            System.out.println("Given string length must be an even number");
-            return "";
-        }
         return cutThis.substring(0,cutThis.length()/2);
     }
 }
