9f46e8648ccacbf8b580b77b0e8d969c7ffda6fd
arturdanielyan1 <arturdanielyan19@gmail.com>
2021-08-17 19:20:41 +0400

    ex2 removed check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(sum(5,5));
+        System.out.println(sum(5,5,5));
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sum(int a, int b , int c){
+        return a+b+c;
+    }
 }
--- src/main/java/homeworks/artur/danielyan/homework3/Exercise2.java
+++ src/main/java/homeworks/artur/danielyan/homework3/Exercise2.java
@@ -2,16 +2,10 @@ package homeworks.artur.danielyan.homework3;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        String str = "Hello!";
-        String extract = str.substring(0,3);
         System.out.println(putIn("1515","yes"));
     }
 
     public static String putIn(String out, String word){
-        if(out.length() != 4){
-            System.out.println("Out String length must be 4");
-            return "";
-        }
         return out.substring(0,2) + word + out.substring(2);
     }
 }
