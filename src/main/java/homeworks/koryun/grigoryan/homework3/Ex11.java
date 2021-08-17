ce688b31158868e8480073eff26acba1d9bfa86e
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-17 20:07:47 +0400

    Committing final solution of exercise 11

--- src/main/java/homeworks/koryun/grigoryan/homework3/Ex11.java
+++ src/main/java/homeworks/koryun/grigoryan/homework3/Ex11.java
@@ -2,13 +2,14 @@ package homeworks.koryun.grigoryan.homework3;
 
 public class Ex11 {
     public static void main(String[] args) {
-        System.out.println(generateString("hippo", "hi"));
+        System.out.println(generateString("hippo", "hippo"));
         System.out.println(generateString("hippo", "xip"));
         System.out.println(generateString("hippo", "him"));
         System.out.println(generateString("hi", "him"));
 
     }
     public static String generateString(String str, String word){
+
         if (word.length()>str.length()){
             return "";
         }
