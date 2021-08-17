1202c7a40d259eee7baa62f3b3180b75fcc402d1
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2021-08-17 20:04:07 +0400

    Improving the solution of exercise 11(Checking if the word is larger than the string)

--- src/main/java/homeworks/koryun/grigoryan/homework3/Ex11.java
+++ src/main/java/homeworks/koryun/grigoryan/homework3/Ex11.java
@@ -5,9 +5,13 @@ public class Ex11 {
         System.out.println(generateString("hippo", "hi"));
         System.out.println(generateString("hippo", "xip"));
         System.out.println(generateString("hippo", "him"));
+        System.out.println(generateString("hi", "him"));
 
     }
     public static String generateString(String str, String word){
+        if (word.length()>str.length()){
+            return "";
+        }
         for(int i = 1; i<str.length() && i<word.length();i++){
             if(word.charAt(i) != str.charAt(i)){
                 return "";
